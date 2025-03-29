46527e81b88e3c75d62502b1d6fa1df8577ce9e8
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-30 00:33:54 +0200

    Implement Catalog page layout with product and recommendation sections

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,27 @@
+import CatalogCardList from "@/components/CatalogCardList/CatalogCardList";
+import { products } from "@/constants/products";
+import styles from "./CatalogPage.module.scss";
 
 export default function Catalog() {
-    return <div>
-      <h1>CATALOG</h1>
-    </div>;
-  }
-  
+  return (
+    <>
+      <section className="container">
+        <h2 className={styles.title}>Всі товари</h2>
+
+        <ul className={styles.productsList}>
+          <CatalogCardList products={products} perRow={4} />
+        </ul>
+
+        <button className={styles.productBtn}>Показати більше</button>
+      </section>
+
+      <section className={`container ${styles.recommendationContainer}`}>
+        <h2 className={styles.title}>Рекомендації</h2>
+
+        <ul className={styles.recommendationList}>
+          <CatalogCardList perRow={3} products={products.slice(0, 3)} />
+        </ul>
+      </section>
+    </>
+  );
+}
