afdebd9c5b5112acff60fac803b7864603465c55
Mogreyn <alv87@bk.ru>
2025-04-19 19:27:25 +0300

    Refactor Footer component structure and styles for improved layout and responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,32 +16,47 @@ const Footer = ({ className }: FooterProps) => {
     <footer className="container">
       <div className={`${combinedClass} ${styles.footer}`}>
         <div className={styles.footer_container}>
-          <Link href="/" className={styles.linkLogo}>
-            <Image
-              src="/images/logo/logo.svg"
-              alt="Логотип компании"
-              width={64}
-              height={64}
-              sizes="(max-width: 768px) 48px, 48px"
+          <div className={styles.logo}>
+            <Link href="/" className={styles.linkLogo}>
+              <Image
+                src="/images/logo/logo.svg"
+                alt="Логотип компанії"
+                width={64}
+                height={64}
+                sizes="(max-width: 768px) 48px, 48px"
+              />
+            </Link>
+          </div>
+
+          <div className={styles.addressForm}>
+            <AddressForm
+              address={address}
+              phoneNumber={phoneNumber}
+              email={email}
             />
-          </Link>
-          <FooterLinks />
-          <AddressForm
-            address={address}
-            phoneNumber={phoneNumber}
-            email={email}
-          />
-          <SocialMediaHub>
-            <SocialMediaLinks platform={"instagram"} />
-            <SocialMediaLinks platform={"viber"} />
-            <SocialMediaLinks platform={"telegram"} />
-          </SocialMediaHub>
+          </div>
+
+          <div className={styles.footerLinks}>
+            <FooterLinks />
+          </div>
+
+          <div className={styles.socialHub}>
+            <SocialMediaHub>
+              <SocialMediaLinks platform={"instagram"} />
+              <SocialMediaLinks platform={"viber"} />
+              <SocialMediaLinks platform={"telegram"} />
+            </SocialMediaHub>
+          </div>
         </div>
-        <p className={styles.authorRights}>
-          © 2025 RECO. Всі права захищені.
-          <Link href="/policy/privacy-policy">Політика конфіденційності</Link> |
-          <Link href="/policy/offer">Оферта</Link>
-        </p>
+
+        <div className={styles.authorRights}>
+          <p>© 2025 RECO. Всі права захищені.</p>
+          <div className={styles.policyLinks}>
+            <Link href="/policy/privacy-policy">Політика конфіденційності</Link>
+            &nbsp;|&nbsp;
+            <Link href="/policy/offer">Умови використання</Link>
+          </div>
+        </div>
       </div>
     </footer>
   );
