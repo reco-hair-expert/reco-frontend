59ced70d3aeedca95a50d1d1f1594373ed5ff74a
Mogreyn <alv87@bk.ru>
2025-04-26 16:02:50 +0300

    feat: replace products.ts with new product data structure in products.js

---
+++ src/constants/products.js
@@ -0,0 +1,235 @@
+const product =[
+  {
+    "name": "RECO",
+    "photo": "reco",
+    "type": "Реконструктор",
+    "shortDescription": "Супервідновлення волосся за 20 хвилин.",
+    "description": "Інноваційний продукт для відновлення максимально пошкодженого волосся. Відновлює дисульфідні та водневі зв'язки у структурі волосся всього за 20 хвилин.",
+    "application": "Нанести на чисте вологе волосся, залишити на 20 хвилин, змити водою.",
+    "composition": "Aqua (Water), Bis-aminopropyl Diglycol Dimaleate, Sodium Benzonate, Macadamia Oil, Cottonseed-sweet Almond Oil, Olive Oil, Avocado Oil, Soybean Oil, Mango Butter And Cocoa Butter, Cocos Y (Coconut) Oil, Parfum (Fragrance), Phenoxyethanol, Polyquaternium-10, Citric Acid, Glycerin, Serine, Benzyl Salicylate, Tyhreonine, Peg-8, Coumarin, Ci 15510 (Orange 4), Helianthus Annuus (Sunflower) Seed Oil, Ppg-26-buteth-26 + запатентована формула Hair__expert",
+    "recommendation": "Підходить для відновлення сильно пошкодженого волосся.",
+    "sizes": [
+      { "size": "15 ml", "price": 600 },
+      { "size": "30 ml", "price": 1200 },
+      { "size": "50 ml", "price": 1800 },
+      { "size": "100 ml", "price": 3240 },
+      { "size": "200 ml", "price": 6000 }
+    ],
+    "badgeInfo": "Хіт",
+    "isNew": false
+  },
+  {
+    "name": "HAPPY END",
+    "photo": "happyEnd",
+    "type": "Живлення",
+    "shortDescription": "Коктейль для блиску й захисту волосся.",
+    "description": "Коктейль для живлення, зволоження та блиску волосся. Зміцнює, захищає від UV-променів, надає м'якості та шовковистості.",
+    "application": "Нанести на вимите вологе волосся, рівномірно прочесати і залишити на 20 хвилин, ретельно змити водою.",
+    "composition": "Water (Aqua), Bis-Aminopropyl Diglycol Dimaleate, Propylene Glycol, Cetearyl Alcohol, Behentrimonium Methosulfate, Stearamidopropyl Dimethylamine, Hydrolyzed Soy Protein, Hydrolyzed Wheat Protein, Arginine HCL, Panthenol, Tocopheryl Acetate, Ascorbic Acid, Aloe Barbadensis Leaf Juice, Simmondsia Chinensis (Jojoba) Seed Oil, Cocos Nucifera (Coconut) Oil, Macadamia Ternifolia Seed Oil, Persea Gratissima (Avocado) Oil, Phytantriol, Guar Hydroxypropyltrimonium Chloride, Phenoxyethanol.",
+    "recommendation": "Для зволоження, зміцнення та блиску волосся.",
+    "sizes": [
+      { "size": "15 ml", "price": 720 },
+      { "size": "30 ml", "price": 1440 },
+      { "size": "50 ml", "price": 2160 },
+      { "size": "100 ml", "price": 3960 },
+      { "size": "200 ml", "price": 7200 }
+    ],
+    "badgeInfo": "Топ",
+    "isNew": false
+  },
+  {
+    "name": "VOL UP",
+    "photo": "volUp",
+    "type": "Об'єм",
+    "shortDescription": "Спрей-об'єм з УФ-захистом і доглядом.",
+    "description": "Спрей для прикореневого об'єму, що підходить для всіх типів волосся. Надає приголомшливого об'єму, блиску та має УФ-фільтр.",
+    "application": "Нанести 3-5 натискань спрею з відстані 10-15 см на корені волосся. Розподілити рівномірно, після чого висушити волосся.",
+    "composition": "Alcohol Denat. Polyquaternium-4, (Sd Alcohol 40-b) (Sda-ca-2877), Polysorbate 20, Aqua/Water, Pvp, Octylacrylamide/Acrylates/Butylaminoethyl Methacrylate Copolymer, Panthenol, Hedychium Coronarium Flower/Leaf/Stem Extract, Aminomethyl Propanol, Peg/ Ppg-18/18 Dimethicone, Triethyl Citrate, Betaine, Glycerin, Isopropyl Alcohol, Bisamino Peg/Ppg-41/3 Aminoethyl Pg-propyl, Peg-12, Sodium Chloride, Parfum (Fragrance), Benzyl Benzoate, Benzyl Alcohol, Hydroxycitronellal, Citronellol, Linalool, Hexyl Cinnamal, Cinnamyl Alcohol, Amyl Cinnamal, Butylphenyl Methylpropional.",
+    "recommendation": "Підходить для всіх типів волосся для створення об'єму та блиску.",
+    "sizes": [
+      { "size": "100 ml", "price": 1080 },
+      { "size": "250 ml", "price": 2160 }
+    ],
+    "badgeInfo": "Топ",
+    "isNew": false
+  },
+  {
+    "name": "SHAMPOO VOL",
+    "photo": "shampooVol",
+    "type": "Шампунь",
+    "shortDescription": "Шампунь для об'єму",
+    "description": "Шампунь для об'єму та зміцнення волосся. Делікатно очищує, надає прикореневий об'єм, зміцнює структуру та покращує еластичність.",
+    "application": "Нанести невелику кількість шампуню на вологе волосся, спінити масажними рухами, змити теплою водою. За потреби повторити.",
+    "composition": "Aqua, Sodium Laureth Sulfate, Decyl Glucoside, Cocamidopropyl Betaine, Glycerin, Hydrolyzed Wheat Protein, Panthenol, Chenopodium Quinoa Seed Extract, Simmondsia Chinensis (Jojoba) Seed Oil, Argania Spinosa Kernel Oil, Polyquaternium-10, Citric Acid, Sodium Chloride, Sodium Benzoate, Potassium Sorbate, Lactic Acid.",
+    "recommendation": "Для тонкого та ослабленого волосся, підходить для щоденного використання.",
+    "sizes": [
+      { "size": "100 ml", "price": 450 },
+      { "size": "250 ml", "price": 900 },
+      { "size": "1000 ml", "price": 2700 }
+    ],
+    "badgeInfo": "Топ",
+    "isNew": false
+  },
+  {
+    "name": "CONDITIONER VOL",
+    "photo": "conditionerVol",
+    "type": "Кондиціонер",
+    "shortDescription": "Кондиціонер для об'єму",
+    "description": "Легкий кондиціонер для тонкого та ослабленого волосся, що надає прикореневий об'єм без обтяження. Зволожує, зміцнює, відновлює структуру волосся.",
+    "application": "Нанести невелику кількість на чисте вологе волосся, розподілити по всій довжині, уникаючи коренів. Залишити на 2-3 хвилини, змити теплою водою.",
+    "composition": "Aqua (Water), Cetearyl Alcohol, Dicetyldimonium Chloride, Cetrimonium Chloride, Polyquaternium-37, Hydrolyzed Wheat Protein, Cocodimonium Hydroxypropyl Hydrolyzed Wheat Protein, Aloe Barbadensis (Aloe Vera) Extract, Simmondsia Chinensis (Jojoba) Seed Extract, Rosmarinus Officinalis (Rosemary) Extract, Anthemis Nobilis (Roman Chamomile) Flower Extract, Lawsonia Inermis (Henna) Extract, Hydrolyzed Wheat Starch, Panthenol, Phenoxyethanol, Chlorhexidine Digluconate.",
+    "recommendation": "Для тонкого та ослабленого волосся, підходить для щоденного використання.",
+    "sizes": [
+      { "size": "100 ml", "price": 840 },
+      { "size": "250 ml", "price": 1700 },
+      { "size": "1000 ml", "price": 5100 }
+    ],
+    "badgeInfo": "Топ",
+    "isNew": false
+  },
+  {
+    "name": "EveryDay",
+    "photo": "everyDay",
+    "type": "Маска",
+    "shortDescription": "Маска на кожен день: зволоження + відновлення",
+    "description": "Засіб для щоденного догляду за волоссям. Розгладжує, усуває пухнастість і надає природного блиску. Формула з амінокислотами, колагеном та оліями.",
+    "application": "Нанести на чисте вологе волосся, залишити на 3-7 хвилин, змити водою.",
+    "composition": "Aqua (Water), Cetearyl Alcohol, Glycerin, Behentrimonium Chloride, Cetrimonium Chloride, Cocos Nucifera (Coconut) Oil, Prunus Armeniaca (Apricot) Kernel Oil, Panthenol, Propylene Glycol, PEG-40 Hydrogenated Castor Oil, Linoleic Acid, Tocopheryl Acetate, Retinyl Palmitate, Glycine Soya (Soybean) Oil, Tocopherol, Hydrolyzed Collagen, Helianthus Annuus (Sunflower) Seed Oil, Carbocysteine, Phenoxyethanol, Potassium Sorbate, Sodium Benzoate, Methylchloroisothiazolinone, Methylisothiazolinone, Lactic Acid.",
+    "recommendation": "Для щоденного догляду за волоссям, забезпечує гладкість і блиск.",
+    "sizes": [
+      { "size": "50 ml", "price": 600 },
+      { "size": "100 ml", "price": 1140 },
+      { "size": "200 ml", "price": 2160 }
+    ],
+    "badgeInfo": null,
+    "isNew": false
+  },
+  {
+    "name": "SHAMPOO HYDRATING",
+    "photo": "shampooHydrating",
+    "type": "Шампунь",
+    "shortDescription": "Зволожуючий шампунь",
+    "description": "Шампунь для глибокого зволоження та делікатного догляду за сухим і пошкодженим волоссям. Формула з пантенолом, протеїнами шовку та керамідами.",
+    "application": "Нанести невелику кількість шампуню на вологе волосся, спінити масажними рухами, змити теплою водою. За потреби повторити.",
+    "composition": "Aqua, Sodium Cocoyl Isethionate, Cocamidopropyl Betaine, Sodium Lauroyl Methyl Isethionate, Panthenol, Betaine, Hydrolyzed Silk Protein, Ceramide NP, Aloe Barbadensis Leaf Extract, Simmondsia Chinensis Seed Oil, Argania Spinosa Kernel Oil, Hydrolyzed Keratin, Polyquaternium-10, Sodium Benzoate, Potassium Sorbate, Lactic Acid.",
+    "recommendation": "Для сухого та пошкодженого волосся, підходить для щоденного використання.",
+    "sizes": [
+      { "size": "100 ml", "price": 450 },
+      { "size": "250 ml", "price": 900 },
+      { "size": "1000 ml", "price": 2700 }
+    ],
+    "badgeInfo": null,
+    "isNew": false
+  },
+  {
+    "name": "DETOX SHAMPOO",
+    "photo": "shampooDetox",
+    "type": "Шампунь",
+    "shortDescription": "Шампунь детоксикації",
+    "description": "Шампунь глибокого очищення, який ефективно видаляє залишки стайлінгу, надлишки себуму, пил та токсини. Очищує без пересушування.",
+    "application": "Нанести невелику кількість шампуню на вологе волосся, спінити масажними рухами, особливо приділяючи увагу кореням. Змити теплою водою. За потреби повторити.",
+    "composition": "Aqua, Disodium Laureth Sulfosuccinate, Cocamidopropyl Betaine, Decyl Glucoside, Glyceryl Oleate, Aloe Barbadensis Leaf Juice, Panthenol, Chamomilla Recutita (Matricaria) Flower Extract, Simmondsia Chinensis (Jojoba) Seed Oil, Rosmarinus Officinalis (Rosemary) Leaf Extract, Hydrolyzed Rice Proten Citric Acid, Sodium Phytate, Sodium Benzoate, Potassium Sorbate Parfum (Natural Essential Oils), Linalool, Limonene",
+    "recommendation": "Для жирної шкіри голови та підготовки до доглядових процедур.",
+    "sizes": [
+      { "size": "100 ml", "price": 495 },
+      { "size": "250 ml", "price": 990 },
+      { "size": "1000 ml", "price": 2970 }
+    ],
+    "badgeInfo": null,
+    "isNew": false
+  },
+  {
+    "name": "DETOX NATURAL SCRUB",
+    "photo": "detoxScrub",
+    "type": "Скраб",
+    "shortDescription": "Скраб для шкіри голови",
+    "description": "Натуральний скраб для глибокого очищення шкіри голови. Видаляє відмерлі клітини, стимулює кровообіг та покращує ріст волосся.",
+    "application": "Нанести на вологу шкіру голови, масажувати 2-3 хвилини, змити водою.",
+    "composition": "Sodium Chloride, Cocos Nucifera (Coconut) Oil, Prunus Amygdalus Dulcis (Sweet Almond) Oil, Simmondsia Chinensis (Jojoba) Seed Oil, Melaleuca Alternifolia (Tea Tree) Leaf Oil, Citrus Limon (Lemon) Peel Oil, Mentha Piperita (Peppermint) Oil, Tocopherol.",
+    "recommendation": "Для всіх типів шкіри голови, особливо при лущенні та жирності.",
+    "sizes": [
+      { "size": "100 г", "price": 570 }
+    ],
+    "badgeInfo": "NEW",
+    "isNew": true
+  },
+  {
+    "name": "REC ON Booster",
+    "photo": "recoOn",
+    "type": "Реконструктор",
+    "shortDescription": "Спрей-реконструктор для відновлення волосся.",
+    "description": "Спрей-реконструктор на основі унікальної формули RECO, що глибоко живить і відновлює структуру волосся. Ідеально підходить для пористого, жорсткого, ламкого та пошкодженого волосся.",
+    "application": "Нанести спрей на всю довжину волосся, відступивши від кореня 2-3 см. Рівномірно розподілити, зосереджуючись на пошкоджених ділянках.",
+    "composition": "Aqua (Water), Hydrolysed Keratin, Bis-aminopropyl Diglycol, Hydrolyzed Silk Protein, Aspartic Acid, Phenoxyethanol, Citric Acid, Serine, Threonine, Benzyl Salicylate, Sodium Pca, Sodium Lactate, Sodium Benzoate, Valine, Alanine, Proline, Isoleucine, Ethylhexylglycerin, Beta-carotene.",
+    "recommendation": "Цей продукт ідеально підходить для волосся, яке стало пористим, жорстким, ламким та пошкодженим, втративши свою природну енергію.",
+    "sizes": [
+      { "size": "100 ml", "price": 1560 },
+      { "size": "250 ml", "price": 3120 }
+    ],
+    "badgeInfo": null,
+    "isNew": false
+  },
+  {
+    "name": "TERMO CONTROL",
+    "photo": "termoControl",
+    "type": "Термозахист",
+    "shortDescription": "Термозахисний спрей RECO для блиску і захисту.",
+    "description": "Універсальний термозахисний спрей з формулою RECO. Захищає волосся від високих температур, ультрафіолету, розгладжує кутикулу, зменшує пухнастість.",
+    "application": "Струсити флакон, рівномірно нанести на волосся, не змивати.",
+    "composition": "Aqua/Water/Eau, Coconut Oil, Pantenol, Amodimethicone, Polyquaternium-37, Propylene Glycol, Xylitylglucoside, Sodium Hydroxide, Anhydroxylitol, Xylitol, Dimethi-cone Peg-7 Phosphate, Ppg-1 Trideceth-6, Behentrimonium Chloride, Limonene, Xylose, Ethylhexylglycerin, Sorbitan Oleate, Isopropyl Alcohol, Cetrimonium Chloride, Glycerin, Hydrolyzed Vegetable Protein Pg-propyl Silanetriol, Benzyl Salicylate, Coumarin, Zingiber Officinale Root Extract/ Ginger Root Extract, Citral, Benzyl Alcohol, Citronellol, Leontopodium Alpinum Callus Culture Extract, Tocopherol, Citric Acid, Xanthan Gum, Dipeptide Diaminobutyroyl Benzylamide Diacetate, Parfum/fragrance.",
+    "recommendation": "Для захисту волосся перед укладанням феном, праскою або плойкою.",
+    "sizes": [
+      { "size": "100 ml", "price": 1440 },
+      { "size": "250 ml", "price": 2880 }
+    ],
+    "badgeInfo": "Хіт",
+    "isNew": false
+  },
+  {
+    "name": "RecOil",
+    "photo": "recoOil",
+    "type": "Олія",
+    "shortDescription": "Живильна олійка",
+    "description": "Коктейль олій з екстрактами макадамії, інки, аргану, лісового горіха та інших натуральних компонентів. Має зволожуючі та поживні властивості.",
+    "application": "Нанести 1-2 краплі на вологе або сухе волосся для додаткового блиску та зволоження.",
+    "composition": "Macadamia Integrifolia Seed Oil, Plukenetia Volubilis Seed Oil, Argania Spinosa Kernel Oil, Corylus Avellana (Hazel) Seed Oil, Gossypium Herbaceum (Cotton) Seed Oil, Carapa Guianensis Seed Oil, Pentaclethra Macroloba Seed Oil, Mauritia Flexuosa Fruit Oil, Moringa Oleifera Seed Oil, Caryodendron Orinocense Seed Oil.",
+    "recommendation": "Для зволоження та захисту волосся від завивання.",
+    "sizes": [
+      { "size": "15 ml", "price": 720 },
+      { "size": "30 ml", "price": 1440 }
+    ],
+    "badgeInfo": null,
+    "isNew": false
+  },
+  {
+    "name": "B3SHOT",
+    "photo": "b3shot",
+    "type": "Спрей",
+    "shortDescription": "Спрей для росту волосся",
+    "description": "Інтенсивний спрей для стимуляції росту волосся з ніацинамідом та біотином. Покращує кровообіг шкіри голови та активізує \"сплячі\" фолікули.",
+    "application": "Наносити на чисті корені волосся 2-3 рази на тиждень, не змивати.",
+    "composition": "Aqua (Water), Niacinamide, Biotin, Panthenol, Aloe Barbadensis Leaf Juice, Hydrolyzed Wheat Protein, Caffeine, Menthol, Arginine, Glycine, Serine, Proline, Phenoxyethanol, Ethylhexylglycerin.",
+    "recommendation": "Для посилення росту волосся та зменшення випадіння.",
+    "sizes": [
+      { "size": "100 ml", "price": 1150 }
+    ],
+    "badgeInfo": "NEW",
+    "isNew": true
+  },
+  {
+    "name": "HOTCHILI OIL",
+    "photo": "hotchiliOil",
+    "type": "Олія",
+    "shortDescription": "Олія для стимуляції росту",
+    "description": "Інтенсивна олія з екстрактом червоного перцю для активізації росту волосся. Насичує корисними речовинами та покращує мікроциркуляцію.",
+    "application": "Нанести на корені волосся за 30 хвилин до миття, масажувати 5 хвилин.",
+    "composition": "Ricinus Communis (Castor) Seed Oil, Capiscum Annuum (Chili) Extract, Rosmarinus Officinalis (Rosemary) Leaf Oil, Olea Europaea (Olive) Fruit Oil, Cocos Nucifera (Coconut) Oil, Argania Spinosa Kernel Oil, Tocopherol.",
+    "recommendation": "Для прискорення росту волосся та зміцнення коренів.",
+    "sizes": [
+      { "size": "100 ml", "price": 1450 }
+    ],
+    "badgeInfo": "NEW",
+    "isNew": true
+  }
+]
+export default product;
