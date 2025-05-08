179f7ea169c4839a5dd52bab8abf16631a662872
Sosnovskyi <roman97039703@gmail.com>
2025-05-08 19:40:37 +0200

    test

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,27 +1,61 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Quiz from "@/components/Quiz/Quiz";
 import { quizData } from "@/constants/quizData";
 import styles from "./QuizPage.module.scss";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+const QuizPopup = dynamic(() => import("@/components/Popup/QuizPopup"), {
+  ssr: false
+});
 
 export default function QuizPage() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [quizStarted, setQuizStarted] = useState(false);
+
   const handleQuizComplete = (results: {
     recommendedProducts: { name: string; score: number }[];
   }) => {
     console.log("Рекомендовані продукти:", results.recommendedProducts);
-    // Тут можна додати логіку перенаправлення або збереження результатів
-    // Наприклад:
-    // localStorage.setItem('quizResults', JSON.stringify(results));
-    // або перенаправлення на сторінку результатів
-    // redirect('/results');
+  };
+
+  useEffect(() => {
+    setIsPopupOpen(true);
+    document.body.style.overflow = isPopupOpen ? "hidden" : "auto";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isPopupOpen]);
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+    setQuizStarted(true);
   };
 
   return (
-    <main className="container">
-      <div className={styles.quizWrapper}>
-        <Quiz data={quizData} onComplete={handleQuizComplete} />
-      </div>
+    <main className={styles.container}>
+      {!quizStarted ? (
+        <>
+          <div className={styles.backgroundImage}>
+            <Image
+              fill
+              alt="happy end"
+              src="/images/sections/quiz/RecoOn.png"
+              quality={100}
+              priority
+              sizes="(max-width: 480px) 100vw, (max-width: 768px) 90vw, 70vw"
+            />
+          </div>
+          <QuizPopup isVisible={isPopupOpen} onClose={handleClosePopup} />
+        </>
+      ) : (
+        <div className={styles.quizWrapper}>
+          <Quiz data={quizData} onComplete={handleQuizComplete} />
+        </div>
+      )}
     </main>
   );
 }
