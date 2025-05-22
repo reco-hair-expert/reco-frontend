5f86b6e82595a146da6c8a690bdb228e741bdcef
Mogreyn <alv87@bk.ru>
2025-05-22 17:34:52 +0300

    Enhance ContactsPage layout and styles; update FeedbackSection design
    - Improved responsiveness of ContactsPage by adding flex properties and adjusting margins.
    - Replaced text icons with image icons for better visual appeal in contact cards.
    - Updated FeedbackSection styles, including increased border-radius for a softer look.
    - Removed unused test files for FeedbackForm and FeedbackSection to streamline the codebase.
    - Updated contact information constants for clarity and consistency.

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -8,27 +8,71 @@ export default function ContactsPage() {
       <div className={styles.contactsContainer}>
         <div className={styles.contacts}>
           <div className={styles.contactCard}>
-            📞 <a href={CONTACTS.phoneHref} className={styles.link}>
+            <div className={styles.imgContainer}>
+              <img
+                src="/icon/C_phone.svg"
+                alt="Phone Icon"
+                className={styles.icon}
+              />
+            </div>
+            <a href={CONTACTS.phoneHref} className={styles.link}>
               {CONTACTS.phoneNumber}
             </a>
           </div>
 
           <div className={styles.contactCard}>
-            📧 <a href={CONTACTS.emailHref} className={styles.link}>
+            <div className={styles.imgContainer}>
+              <img
+                src="/icon/C_mail.svg"
+                alt="Phone Icon"
+                className={styles.icon}
+              />
+            </div>
+            <a href={CONTACTS.emailHref} className={styles.link}>
               {CONTACTS.email}
             </a>
           </div>
 
           <div className={styles.contactCard}>
-            📍 {CONTACTS.address}
+            <div className={styles.imgContainer}>
+              <img
+                src="/icon/C_adr.svg"
+                alt="Phone Icon"
+                className={styles.icon}
+              />
+            </div>
+            {CONTACTS.address}
+          </div>
+          <div className={styles.contactCard}>
+            <div className={styles.imgContainer}>
+              <img
+                src="/icon/C_time.svg"
+                alt="Phone Icon"
+                className={styles.icon}
+              />
+            </div>
+            {CONTACTS.time}
           </div>
 
           <div className={styles.contactCard}>
-            📷 <a href={CONTACTS.instagramHref} className={styles.link} target="_blank" rel="noopener noreferrer">
+            📷{" "}
+            <a
+              href={CONTACTS.instagramHref}
+              className={styles.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {CONTACTS.instagramName}
             </a>
-            <br />
-            💬 <a href={CONTACTS.telegramHref} className={styles.link} target="_blank" rel="noopener noreferrer">
+          </div>
+          <div className={styles.contactCard}>
+            💬{" "}
+            <a
+              href={CONTACTS.telegramHref}
+              className={styles.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {CONTACTS.telegramName}
             </a>
           </div>
