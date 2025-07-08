d41a7b6aac18df9fe86e40c039f3919684ffe84f
Mogreyn <alv87@bk.ru>
2025-07-08 19:33:42 +0300

    Update contact information in constants for accuracy

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,12 +1,12 @@
 export const CONTACTS = {
-  phoneNumber: "+38 (099) 628 45 44",
-  phoneHref: "tel:+380996284544",
+  phoneNumber: "+38 (099) 152 79 18",
+  phoneHref: "tel:+380991527918",
   email: "reco.hair.expert@gmail.com",
   emailHref: "mailto:reco.hair.expert@gmail.com",
   instagramName: "Instagram",
   instagramHref: "https://www.instagram.com/hair__expert.reconstructor?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
   telegramName: "Telegram",
-  telegramHref: "https://t.me/+380996284544",
+  telegramHref: "https://t.me/+380991527918",
   address: "Адреса: м. Днiпро, вул. Виконкомівська 40",
   time: "Графiк роботи: 10:00 - 20:00 Без вихiдних"
 };
