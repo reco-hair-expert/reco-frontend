f91be07f06cd891e28df0641e2af9a4a4ecf206a
Mogreyn <alv87@bk.ru>
2025-07-08 19:32:06 +0300

    Update Telegram contact link in contacts constant for accuracy

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -6,7 +6,7 @@ export const CONTACTS = {
   instagramName: "Instagram",
   instagramHref: "https://www.instagram.com/hair__expert.reconstructor?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
   telegramName: "Telegram",
-  telegramHref: "https://t.me/+380509883339",
+  telegramHref: "https://t.me/+380996284544",
   address: "Адреса: м. Днiпро, вул. Виконкомівська 40",
   time: "Графiк роботи: 10:00 - 20:00 Без вихiдних"
 };
